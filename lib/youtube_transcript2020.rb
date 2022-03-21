e4c80647fc59372e2baf7687ef748b783cfa2f26
James Robertson <digital.robertson@gmail.com>
2022-03-21 20:20:35 +0000

    bug fix: RXFReader now replaces RXFHelper. Note: Fetching of transcript from google.com no longer possible.

--- lib/youtube_transcript2020.rb
+++ lib/youtube_transcript2020.rb
@@ -12,21 +12,25 @@ class YoutubeTranscript2020
 
   attr_reader :to_a, :author, :id, :title
 
-  def initialize(id=nil, debug: false)  
+  def initialize(id=nil, debug: false)
 
     return unless id
-    
+
     @debug = debug
 
     @id = id[/https?:\/\//] ? YoutubeID.from(id) : id
 
-    s = Net::HTTP.get(URI("http://video.google.com/timedtext?lang=en&v=#{@id}"))
-    @s = parse(s) unless s.empty?
+    # Fetching the transcript from the following statement no longer works.
+    # Instead, copy and paste the transcript from the YouTube video page into
+    # a text file and import it.
+    #
+    #s = Net::HTTP.get(URI("http://video.google.com/timedtext?lang=en&v=#{@id}"))
+    #@s = parse(s) unless s.empty?
 
     fetch_info(@id)
 
   end
-  
+
   def to_a()
     @a
   end
@@ -38,7 +42,7 @@ class YoutubeTranscript2020
     h = {id: @id, title: @title, author: @author}
     SimpleConfig.new(h).to_s + "\n#{'-'*78}\n\n" + @s
   end
-  
+
   def to_text()
     @a.map(&:last).join("\n")
   end
@@ -47,7 +51,7 @@ class YoutubeTranscript2020
   #
   def import(obj)
 
-    s = RXFHelper.read(obj).first
+    s = RXFReader.read(obj).first
 
     if s =~ /------+/ then
       header, body = s.split(/-----+/,2)
@@ -61,7 +65,7 @@ class YoutubeTranscript2020
     end
 
     puts 'body: ' + body[0..400] if @debug
-    a = body.lines.map(&:chomp).partition {|x| x =~ /\d+:\d+/ }    
+    a = body.lines.map(&:chomp).partition {|x| x =~ /\d+:\d+/ }
     @a = a[0].zip(a[1])
 
     @s = join_sentences(@a) if raw_transcript
@@ -75,13 +79,13 @@ class YoutubeTranscript2020
     url = 'https://www.youtube.com/embed/' + @id
 
     links = @a.map do |timestamp, s|
-      
-      seconds = Subunit.new(units={minutes:60, hours:60}, 
+
+      seconds = Subunit.new(units={minutes:60, hours:60},
                   timestamp.split(':').map(&:to_i)).to_i
       "<li><a href='%s?start=%s&autoplay=1' target='video'>%s</a><p>%s</p></li> " \
           % [url, seconds, timestamp, s]
     end
-    
+
     puts '@html_embed: ' + @html_embed.inspect if @debug
     doc = Rexle.new(@html_embed.to_s)
     puts 'before attributes'
@@ -116,9 +120,9 @@ EOF
   # Outputs plain text containing the headings including timestamps
   # note: This can be helpful for copyng and pasting directly into a YouTube comment
   #
-  def to_headings()    
-    
-    @to_a.select {|timestamp, _| timestamp =~ / /}.map(&:first)    
+  def to_headings()
+
+    @to_a.select {|timestamp, _| timestamp =~ / /}.map(&:first)
 
   end
 
@@ -133,53 +137,53 @@ EOF
   private
 
   def fetch_info(id)
-    
-    url = "http://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=#{id}&format=xml"
+
+    url = "https://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=#{id}&format=xml"
     s = Net::HTTP.get(URI(url))
-    
+
     e = Rexle.new(s).root
-    
+
     @title = e.text('title')
     @author = e.text('author_name')
     @html_embed = e.text('html').unescape
     puts '@html_embed: ' + @html_embed.inspect if @debug
-        
+
   end
-  
+
   def join_sentences(a)
-    
+
     if @debug then
       puts 'inside join_sentence'
       puts 'a: ' + a.take(3).inspect
     end
-    
+
     a2 = []
 
     # the following cleans up sentences that start with And, Or, But, So etc.
 
     (0..a.length - 1).each do |n|
-      
+
       time, s = a[n]
 
-      puts 's: ' + s.inspect if @debug      
-      
+      puts 's: ' + s.inspect if @debug
+
       if s[/^[a-z|0-9]|I\b|I'/] then
-        
+
         if a2.any? then
-          
-          # only join two parts together if there was no full stop in 
+
+          # only join two parts together if there was no full stop in
           # the previous line
-          
+
           if a2[-1][-1] != /\.$/ then
-            a2[-1][-1] = a2[-1][-1].chomp + ' ' + s            
+            a2[-1][-1] = a2[-1][-1].chomp + ' ' + s
           else
             a2 << [time, s]
           end
-          
-        else          
+
+        else
           a2 << [time, s.capitalize]
         end
-        
+
       elsif s[/^And,? /]
         a2[-1][-1] += ' ' + s.sub(/^And,? /,'').capitalize
       elsif  s[/^Or,? /]
@@ -191,40 +195,40 @@ EOF
       elsif s[/^So,? /]
         a2[-1][-1] += ' ' + s.sub(/^So,? /,'').capitalize
       elsif s[/^\[(?:Music|Applause)\]/i]
-        
+
         # ignore it
         puts 'ignoring action commentary' if @debug
         a2 << [time, '.']
-        
-        # To promote the next sentence to a new timestamp we 
+
+        # To promote the next sentence to a new timestamp we
         # capitalize the 1st letter
         a[n+1][-1] = a[n+1][-1].capitalize if a[n+1]
       else
-        
+
         if a2.any? and not a2[-1][-1] =~ /\.\s*$/ then
-            a2[-1][-1] = a2[-1][-1].chomp + ' ' + s            
+            a2[-1][-1] = a2[-1][-1].chomp + ' ' + s
         else
             a2 << [time, s]
         end
-        
+
       end
 
     end
-    
+
     # Remove those modified entries which were labelled [Music] etc
     a2.reject! {|time, s| s.length < 2}
 
     # formats the paragraph with the timestamp appearing above
     @a = a2
-    a2.map {|time, s| "\n%s\n\n%s" % [time, s]}.join("\n")    
-    
+    a2.map {|time, s| "\n%s\n\n%s" % [time, s]}.join("\n")
+
   end
 
   def parse(s)
 
     doc = Rexle.new(s)
 
-    a = doc.root.elements.each.map do |x| 
+    a = doc.root.elements.each.map do |x|
       timestamp = Subunit.new(units={minutes:60, hours:60}, \
         seconds: x.attributes[:start].to_f).to_s(verbose: false)
       [timestamp, x.text.unescape.gsub("\n", ' ').gsub('&#39;',"'").gsub('&quot;','"')]
--- youtube_transcript2020.gemspec
+++ youtube_transcript2020.gemspec
@@ -1,17 +1,17 @@
 Gem::Specification.new do |s|
   s.name = 'youtube_transcript2020'
-  s.version = '0.3.1'
+  s.version = '0.3.2'
   s.summary = 'Makes it easier to digest a Youtube video by ' + 
       'reading the transcript.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/youtube_transcript2020.rb']
-  s.add_runtime_dependency('yawc', '~> 0.2', '>=0.2.0')
-  s.add_runtime_dependency('subunit', '~> 0.5', '>=0.5.2')
-  s.add_runtime_dependency('simple-config', '~> 0.7', '>=0.7.1')
+  s.add_runtime_dependency('yawc', '~> 0.3', '>=0.3.0')
+  s.add_runtime_dependency('subunit', '~> 0.8', '>=0.8.5')
+  s.add_runtime_dependency('simple-config', '~> 0.7', '>=0.7.2')
   s.add_runtime_dependency('youtube_id', '~> 0.1', '>=0.1.0')    
   s.signing_key = '../privatekeys/youtube_transcript2020.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/youtube_transcript2020'
 end
